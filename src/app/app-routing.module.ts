9e381f2bae9319f537fdc93dc38eb73bbd8780a4
rob <rjgallac@hotmail.com>
2021-12-05 18:45:42 +0000

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { DaysComponent } from './days/days.component'
+import { WeekSummaryComponent } from './week-summary/week-summary.component';
+import { MonthSummaryComponent } from './month-summary/month-summary.component'
+const routes: Routes = [
+  {
+    path: '',
+    component: DaysComponent
+  },
+  {
+    path: 'week',
+    component: WeekSummaryComponent
+  },
+  {
+    path: 'month',
+    component: MonthSummaryComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
